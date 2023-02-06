479aba8af0b5254c81c4ba4beb44946ab4c7e26a
Roman <R.rybkin94@gmail.com>
2023-02-06 21:27:32 +0400

    homework 9 new

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -50,14 +50,14 @@ function Clock() {
 
 
     //Настройка дня, месяца, года (скрытое)
-    const formatDate = new Intl.DateTimeFormat("en-US",{
-        day: "2-digit",
-        month: 'numeric',
-        year: "numeric"
-    })
+    // const formatDate = new Intl.DateTimeFormat("en-US",{
+    //     day: "2-digit",
+    //     month: 'numeric',
+    //     year: "numeric"
+    // })
 
     // date.toLocaleDateString()
-    const stringDate =  formatDate.format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = date.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     // const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02
